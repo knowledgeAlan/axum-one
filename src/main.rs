3bd69c3a80fd8ac38b1979d44c7e41613f8dfa7d
knowledgeAlan <knowledgeAlan>
2024-07-17 17:25:57 +0800

    path rest

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
  
 
-use axum::{extract::Query, response::{Html, IntoResponse}, routing::{get, Router}};
+use axum::{extract::{Path, Query}, response::{Html, IntoResponse}, routing::{get, Router}};
 use serde::Deserialize;
 use std::net::SocketAddr;
  
@@ -12,7 +12,9 @@ struct HelloParams {
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello",get(handler_hello));
+    let routes_hello = Router::new()
+    .route("/hello",get(handler_hello))
+    .route("/hello2/:name", get(handler_hello2));
 
 
     let addr = SocketAddr::from(([127, 0, 0, 1],8080));
@@ -31,4 +33,10 @@ async fn handler_hello(Query(params):Query<HelloParams>) -> impl IntoResponse {
 
     let name =  params.name.as_deref().unwrap_or("world");
     return Html(format!("Hello {name} world"));
+}
+
+async fn handler_hello2(Path(name) : Path<String>) -> impl IntoResponse{
+    println!("->> {:12} -handler_hello2 - {name:?}","handler");
+
+    Html(format!("hello2 {name}"))
 }
