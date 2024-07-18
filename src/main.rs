c8d715ba0e2090e3f0e1129c3753b68d989a13e8
knowledgeAlan <knowledgeAlan>
2024-07-18 17:01:39 +0800

    static file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
  
 
-use axum::{extract::{Path, Query}, response::{Html, IntoResponse}, routing::{get, Router}};
+use axum::{extract::{Path, Query}, response::{Html, IntoResponse}, routing::{get, get_service, Router}};
 use serde::Deserialize;
+use tower_http::services::ServeDir;
 use std::net::SocketAddr;
- 
-
+pub use self::error::{Error, Result};
+mod error;
+mod web;
 #[derive(Debug,Deserialize)]
 struct HelloParams {
     name: Option<String>,
@@ -13,8 +15,9 @@ struct HelloParams {
 #[tokio::main]
 async fn main() {
     let routes_hello = Router::new()
-    .route("/hello",get(handler_hello))
-    .route("/hello2/:name", get(handler_hello2));
+    .merge(routes_hello())
+    .fallback_service(routes_static())
+    ;
 
 
     let addr = SocketAddr::from(([127, 0, 0, 1],8080));
@@ -26,6 +29,16 @@ async fn main() {
                 .await
                 .unwrap();
 
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+fn routes_hello() -> Router {
+    Router::new()  
+    .route("/hello",get(handler_hello))
+    .route("/hello2/:name", get(handler_hello2))
 }
 
 async fn handler_hello(Query(params):Query<HelloParams>) -> impl IntoResponse {
